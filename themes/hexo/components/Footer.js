fa360318c3b874c7cd597862e4a0554a22d2e245
USBrain <119862489+notfaye2002@users.noreply.github.com>
2024-08-17 00:11:23 +0800

    Update Footer.js

--- themes/hexo/components/Footer.js
+++ themes/hexo/components/Footer.js
@@ -12,16 +12,16 @@ const Footer = ({ title }) => {
     >
       {/* <DarkModeButton/> */}
 
-      <i className='fa-regular fa-copyright' /> {`${copyrightDate}`} <span> <a href={siteConfig('LINK')} className='underline font-bold  dark:text-gray-300 '>{siteConfig('AUTHOR')}</a>.<br/>
+      <i className='fa-regular fa-copyright' /> {`${copyrightDate}`} <span> <a href={siteConfig('LINK')}>{siteConfig('AUTHOR')}</a>.<br/>
 
       {siteConfig('BEI_AN') && <><i className='fas fa-shield-alt' /> <a href='https://beian.miit.gov.cn/' className='mr-2'>{siteConfig('BEI_AN')}</a><br/></>}
-      /*
-      访问量
+      {/* 网站访问统计 */}
+      {/*
       <span className='hidden busuanzi_container_site_pv'>
             <i className='fas fa-eye'/><span className='px-1 busuanzi_value_site_pv'> </span>  </span>
       <span className='pl-2 hidden busuanzi_container_site_uv'>
         <i className='fas fa-users'/> <span className='px-1 busuanzi_value_site_uv'> </span> </span>
-      */
+      */}
         <p className='text-xs pt-2 text-light-500 dark:text-gray-500'>L'Absurdité règne et l'amour en sauve.</p></span><br/>
 
     </footer>
